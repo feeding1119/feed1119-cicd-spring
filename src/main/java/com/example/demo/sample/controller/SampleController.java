fe55255818179d438c073f7aa903b63d86e2cb7f
kjh <akkaa1119@naver.com>
2024-05-21 19:01:18 +0900

    테스트용 db 연결

--- src/main/java/com/example/demo/sample/controller/SampleController.java
+++ src/main/java/com/example/demo/sample/controller/SampleController.java
@@ -1,16 +1,20 @@
 package com.example.demo.sample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
-public class sampleController {
+public class SampleController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/sample")
     public String getTestSample(){
+        userRepository.save(new User());
         return "테스트용";
     }
 }
---
+++ src/main/java/com/example/demo/sample/controller/User.java
@@ -0,0 +1,16 @@
+package com.example.demo.sample.controller;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="userId", nullable = false)
+    private Long userId;
+}
